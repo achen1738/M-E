db69d5bb77f7ebe4c38f117e2a136006ab75bcb6
achen1738 <ac99@rice.edu>
2019-05-13 08:07:57 -0400

    Updates

---
+++ precache-manifest.bc2cdf9edd4b5a1d9fd0f0d68f2f2fa5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e485291d13e0517b129c2ad3a2c072",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "c65486ad8d7bf07954e7",
+    "url": "/frontend/static/css/main.490d6cb3.chunk.css"
+  },
+  {
+    "revision": "6c6d2779f8026804bf39",
+    "url": "/frontend/static/js/2.66d632cd.chunk.js"
+  },
+  {
+    "revision": "c65486ad8d7bf07954e7",
+    "url": "/frontend/static/js/main.ce773881.chunk.js"
+  },
+  {
+    "revision": "dbd924a9862891b98c97",
+    "url": "/frontend/static/js/runtime~main.f332d61a.js"
+  },
+  {
+    "revision": "c2c15dd7107955cba85c8bdc25f36e72",
+    "url": "/frontend/static/media/ArthurChenResume.c2c15dd7.pdf"
+  },
+  {
+    "revision": "20259bce20327c1f975f0c05015ea73f",
+    "url": "/frontend/static/media/cells.20259bce.jpg"
+  },
+  {
+    "revision": "ff3ea3c53db100b61ac84e248a3f38bf",
+    "url": "/frontend/static/media/go.ff3ea3c5.jpg"
+  },
+  {
+    "revision": "713dcab773ac6b40bb4d1fc7407381e9",
+    "url": "/frontend/static/media/newLane2.713dcab7.gif"
+  },
+  {
+    "revision": "c5f187dbd2136c714e4f6abf40b96d48",
+    "url": "/frontend/static/media/sem.c5f187db.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ea61505b2a5114f5eb62e1bbfb94f21.js"
+  "/frontend/precache-manifest.bc2cdf9edd4b5a1d9fd0f0d68f2f2fa5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
